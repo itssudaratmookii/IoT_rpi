fce1ee0cadddc25e00c861abe0e375a74ae5e57e
itssudaratmookii <sudaratto65@nu.ac.th>
2022-12-09 11:24:53 +0700

    Moved the status button into its own class with the toggle function moving to method.

--- src/app/main_gui.py
+++ src/app/main_gui.py
@@ -1,49 +1,61 @@
-#Copyright (c) 2022 Sudarat Tokampang <sudaratto65@nu.ac.th>
+# Copyright (c) 2022 Sudarat Tokampang <sudaratto65@nu.ac.th>
 
 """ Document string (Doc String)
-A graphical user interfacr to observe sensor data
-from a remotr sensor through a matt communication channel
+A graphical user interface to observe sensor data
+from a remote sensor through a matt communication channel
 """
 
 __author__ = "Sudarat Tokampang"
 
-# stand
-import tkinter
-
-# make a variable to change color
-color ='green' # =assignment
-
-# made a python function to print "Hello World"
-# def - define
-def hello_world():
-    global color
-    print("Hello World")
-    if color == 'red':
-       color ='green'
-    elif color =='green':
-         color ='yellow'
-    elif color == 'yellow':
-         color = 'red'
-
-    canvas.itemconfig(circle, fill=color)
+# standard libra
+import tkinter as tk
 
 
+class StatusButton:
+    """ Display the status using a canvas
 
-app = tkinter .Tk() #application a class ot tkinter.Tk
+    Attributes:
+        circle: object used to display the status
+        canvas (tk.Canvas): canvas the circle is in
+        color (str): color the circle will show
+
+    """
+
+    def __init__(self, parent):
+        # make a variable to change color
+        self.color = 'red'  # =assignment
+        self.canvas = tk.Canvas(parent, width=120, height=120)
+        self.circle = self.canvas.create_oval(10, 10, 110, 110,
+                                              fill=self.color)
+
+        self.canvas.pack()
+
+    def toggle_color(self):
+        """ Toggle the color between red and green """
+        if self.color == 'red':
+            self.color = 'green'
+        elif self.color == 'green':
+            self.color = 'red'
+        self.canvas.itemconfig(self.circle, fill=self.color)
+
+
+app = tk .Tk()  # application a class ot tkinter.Tk
 # geometry is a method of the tkinter.Tk class that
 # sets the size of the app window. It takes a
-# string as on orgument
-app.geometry ("400x400")
-canvas = tkinter.Canvas(app, width=120, height=120)
-circle = canvas.create_oval(10, 10, 110, 110,
-                            fill= color )
-canvas.pack()
+# string as on argument
+app.geometry("400x400")
+status_btn = StatusButton(app)
+print(status_btn)
+status_btn2 = StatusButton(app)
+print(status_btn2)
 
-# make a botton (tkiner class) and put it in the app
-# botton takes 3 argument, app-where to put the button
+# make a button (tkinter class) and put it in the app
+# button takes 3 argument, app-where to put the button
 # text - key word argument
-tkinter.Button(app, text="Hello World",
-               command=hello_world).pack()
+tk.Button(app, text="Toggle Circle 1",
+          command=status_btn.toggle_color).pack()
+tk.Button(app, text="Toggle Circle 2",
+          command=status_btn2.toggle_color).pack()
 
-app.mainloop() #mainloop is method of tkinter.Tk
-# mathods are function of classes
+app.mainloop()  # mainloop is method of tkinter.Tk
+# methods are function of classes
