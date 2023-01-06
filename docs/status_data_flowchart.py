fc3c73403e3b6368f9c34159890c0c87e2e9a906
itssudaratmookii <sudaratto65@nu.ac.th>
2023-01-06 10:56:05 +0700

    Add unit tests to the project

---
+++ docs/status_data_flowchart.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2022 Sudarat Tokampang <sudaratto65@nu.ac.th>
+
+"""
+Make a flowchart diagramming how that
+status data moves into the program
+"""
+
+
+__author__ = "Sudarat Tokampang"
+
+# installed libraries
+import schemdraw
+
+with
